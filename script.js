9c2f8e0a0aca4ab64a1033fad25d29be28fa835e
Natasha J <njohnson533@gmail.com>
2021-07-13 22:45:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
+const btn = document.querySelector('.populate');
+const btnErase = document.querySelector('.erase');
 
-
-function createGrid(rows, col) {
-    for (let i = 0; i < rows * col; i++){
-        let div = document.createElement('DIV');
-        div.style.borderColor = 'black';
-        div.style.borderWidth = '1px';
-        container.appendChild(div);
+btn.addEventListener("click", function () {
+    const dimension = prompt("how many squares per side?");
+    while ((dimension > 100) && (dimension <= 0)) {
+        prompt("Please choose an integer less than 100");
     }
+        createDivs(dimension, dimension);
+});
+
+btnErase.addEventListener("click", function () {
+    const erase = container.childNodes;
+    for (const node of erase) {
+        node.classList.remove('draw');
+    }
+})
+
+//create divs
+function createDivs(rows, cols) {
+    container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
+    //loop through to create rows*columns number of divs
+    for (let i = 0; i < rows * cols; i++){
+        const div = document.createElement('DIV');
+        div.addEventListener('mouseenter', draw);
+        div.classList.add('child');
+        container.appendChild(div);
+    }   
 }
 
-createGrid(16, 16);
+function draw() {
+    this.classList.add('draw');
+}
+
+
+
+
+
+
+
+
+
+
